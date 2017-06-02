1c0857652d7e390f6b4b78b8e06c390b8409edcf
David Slochower <slochower@gmail.com>
2017-06-02 11:46:03 -0700

    Fix PDF linking to images

--- build/build.sh
+++ build/build.sh
@@ -11,6 +11,9 @@ INPUT_PATH=references/generated/all-sections.md
 
 # Make output directory
 mkdir -p output
+# Move images to output directory
+echo "Copying images"
+cp -R sections/images output/images
 
 # Create HTML outpout
 # http://pandoc.org/MANUAL.html
@@ -27,26 +30,32 @@ pandoc --verbose \
 
 # Create PDF outpout
 echo "Exporting PDF manuscript"
+
+# The reason the images fail is because the PDF creation program
+# is running inside build/ and thus looking there for images.
+# But we want the path to be relative so that the HMTL file,
+# served from gh-pages looks at the relative path.
+
+cd ../output
+
+pandoc --verbose \
+  --from=markdown --to=html5 \
+  --filter pandoc-fignos \
+  --bibliography=../$BIBLIOGRAPHY_PATH \
+  --csl=../$CSL_PATH \
+  --metadata link-citations=true \
+  --css=../output/github-pandoc.css \
+  --katex \
+  --output=../output/nonequilibrium-literature-review.pdf \
+  $INPUT_PATH
+
+# echo "...using LaTeX"
 # pandoc --verbose \
-#   --from=markdown --to=html5 \
+#   --from=markdown \
 #   --filter pandoc-fignos \
 #   --bibliography=$BIBLIOGRAPHY_PATH \
 #   --csl=$CSL_PATH \
 #   --metadata link-citations=true \
-#   --css=github-pandoc.css \
-#   --katex \
 #   --output=output/nonequilibrium-literature-review.pdf \
 #   $INPUT_PATH
-echo "...using LaTeX"
-pandoc --verbose \
-  --from=markdown \
-  --filter pandoc-fignos \
-  --bibliography=$BIBLIOGRAPHY_PATH \
-  --csl=$CSL_PATH \
-  --metadata link-citations=true \
-  --output=output/nonequilibrium-literature-review.pdf \
-  $INPUT_PATH
 
-# Move images to output directory
-echo "Copying images"
-cp -R sections/images output/images
